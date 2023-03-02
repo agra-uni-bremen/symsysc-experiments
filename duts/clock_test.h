9256e089eda081a07d1ade5ead2e1ea952c82221
Karl Aaron Rudkowski <rudkowsk@uni-bremen.de>
2023-03-02 17:41:27 +0100

    Signals and Ports.
    Update symsysc for signal and port mechanism.
    clock_test has signal + sensitive process.

--- duts/clock_test.h
+++ duts/clock_test.h
@@ -7,9 +7,10 @@
 class clock_test : public sc_module {
 public:
     sc_clock m_clock;
+	sc_signal<bool,SC_ONE_WRITER> m_signal;
 
     explicit clock_test(sc_core::sc_module_name name) : sc_module(name),
-    m_clock(sc_clock("clock", sc_time(10, SC_SEC))) {
+    m_clock(sc_clock("clock", sc_time(10, SC_SEC))), m_signal() {
 
         SC_HAS_PROCESS(clock_test);
         SC_METHOD(run_pos);
@@ -17,14 +18,23 @@ public:
 
         SC_METHOD(run_neg);
         sensitive << m_clock.negedge_event();
+
+		SC_METHOD(run_signal);
+		sensitive << m_signal;
     }
 
     void run_pos() {
         std::cout << "posedge clock" << std::endl;
+		m_signal.write(true);
     }
 
     void run_neg() {
         std::cout << "negedge clock" << std::endl;
+		m_signal.write(false);
     }
+
+	void run_signal() {
+		std::cout << "value change on signal" << std::endl;
+	}
 
 };
